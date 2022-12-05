5e22468a5e7d16dfeacf63ef87ca4c7e44728883
KelbonHD <keltontruman@gmail.com>
2022-12-05 11:52:43 -0500

    Primordial Express Addition

--- Projects Page/app.js
+++ Projects Page/app.js
@@ -11,6 +11,8 @@ const clubPic = document.querySelector('.clubPic');
 const clubTitle = document.querySelector('.title-club');
 const portPic = document.querySelector('.portPic');
 const portTitle = document.querySelector('.title-portfolio');
+const primordialPic = document.querySelector('.primordialPic');
+const primordialTitle = document.querySelector('.title-primordial');
 
 burgerToggle.addEventListener('click', () => {
     burgerToggle.classList.toggle('active');
@@ -28,3 +30,4 @@ hover(excursionPic, excursionTitle);
 hover(teaPic, teaTitle);
 hover(clubPic, clubTitle);
 hover(portPic, portTitle);
+hover(primordialPic, primordialTitle);
